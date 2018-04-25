e50470b4a0fac2e24ac589769bfbc2d1f4e9362a
liuchunhua1 <chunhua1@eastmoney.com>
2018-04-25 17:31:07 +0800

    图片编码

---
+++ utility/imageExtension.js
@@ -0,0 +1,34 @@
+var fs = require("fs");
+var path = require('path');
+
+
+function imageEncodeBase64(imageName) {
+    imageDir = __dirname + '/' + imageName
+    imageBuf = fs.readFileSync(imageDir)
+
+    suffix = imageDir.split(".")[1];
+    prefix = "data:" + suffix + ";base64,";
+    base64 = new Buffer(imageBuf, 'binary').toString('base64');
+    imageBase64Result = prefix + base64
+    console.log(imageBase64Result)
+    return imageBase64Result
+}
+
+function decodeBase64Image(data) {
+    var matches = dataString.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
+        response = {};
+
+    if (matches.length !== 3) {
+        return new Error('Invalid input string');
+    }
+
+    response.type = matches[1];
+    response.data = new Buffer(matches[2], 'base64');
+
+    return response;
+}
+
+module.exports = {
+    imageEncodeBase64,
+    decodeBase64Image
+}
